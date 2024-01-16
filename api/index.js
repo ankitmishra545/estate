f26b71eb670acfb60bf3c9ae41ab6fa02fce57ed
Ankit <ankitmishra545@gmail.com>
2024-01-16 18:27:55 +0530

    added preinstallation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/authentication.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_DB).then(() => {
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO_DB).then(() => {
 }).catch((err) => {
     console.log(err)
 }) 
+
+const __dirName = path.resolve();
 
 const app = express();
 
@@ -26,6 +29,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirName, '/client/dist')));
+
+app.get('*',(req, res) => {
+    res.sendFile(path.join(__dirName, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
